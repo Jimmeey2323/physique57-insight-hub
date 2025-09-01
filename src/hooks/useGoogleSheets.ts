062e29457457c94dd42251015d6e081e15908497
Jimmeey2323 <155805143+Jimmeey2323@users.noreply.github.com>
2025-09-01 23:13:32 +0530

    Update useGoogleSheets.ts

--- src/hooks/useGoogleSheets.ts
+++ src/hooks/useGoogleSheets.ts
@@ -3,14 +3,14 @@ import { useState, useEffect } from 'react';
 import { SalesData } from '@/types/dashboard';
 
 const GOOGLE_CONFIG = {
-  CLIENT_ID: "416630995185-007ermh3iidknbbtdmu5vct207mdlbaa.apps.googleusercontent.com",
-  CLIENT_SECRET: "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",
-  REFRESH_TOKEN: "1//04MmvT_BibTsBCgYIARAAGAQSNwF-L9IrG9yxJvvQRMLPR39xzWSrqfTVMkvq3WcZqsDO2HjUkV6s7vo1pQkex4qGF3DITTiweAA",
-  TOKEN_URL: "https://oauth2.googleapis.com/token"
-};
+        CLIENT_ID: "416630995185-g7b0fm679lb4p45p5lou070cqscaalaf.apps.googleusercontent.com",
+        CLIENT_SECRET: "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",
+        REFRESH_TOKEN: "1//0gT2uoYBlNdGXCgYIARAAGBASNwF-L9IrBK_ijYwpce6-TdqDfji4GxYuc4uxIBKasdgoZBPm-tu_EU0xS34cNirqfLgXbJ8_NMk",
+        TOKEN_URL: "https://oauth2.googleapis.com/token"
+    },
 
 
-const SPREADSHEET_ID = "149ILDqovzZA6FRUJKOwzutWdVqmqWBtWPfzG3A0zxTI";
+const SPREADSHEET_ID = "12xbYJQrh5wyYDaFhQrq4L0-YkSSlA6z7nMCb66XEbCQ";
 
 export const useGoogleSheets = () => {
   const [data, setData] = useState<SalesData[]>([]);
